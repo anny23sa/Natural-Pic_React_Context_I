1c688bbf062d2b22c1e0704ba5b00790e05cbd3e
Anny Stecca <anny23sa@gmail.com>
2023-11-01 23:21:56 -0700

    index.css

--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,12 +1,11 @@
-
 import React, { useContext } from "react";
 import { ContextImg } from "../components/ContextImg";
 import IconHeart from "../components/IconHeart";
 
+
 const Favorites = () => {
   const { images } = useContext(ContextImg);
 
-  // Filtrar las imágenes favoritas
   const favoritas = images.filter((image) => image.liked);
 
   return (
@@ -15,9 +14,11 @@ const Favorites = () => {
       <div className="p-3 gallery grid-columns-4">
         {favoritas.map((image, id) => (
           <div key={id} className="image-container">
-            <img src={image.src.tiny} alt={`Imagen ${id + 1}`} />
-            <div className="heart-icon">
-              <IconHeart filled={true} />
+            <div className="image-wrapper">
+              <img src={image.src.tiny} alt={`Imagen ${id + 1}`} />
+              <div className="heart-icon">
+                <IconHeart filled={true} />
+              </div>
             </div>
           </div>
         ))}
